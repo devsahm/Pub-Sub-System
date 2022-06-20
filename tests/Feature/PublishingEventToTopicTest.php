721e28b06e9bfb3357716281b6861c9c6a11c37f
unknown <devsahm>
2022-06-20 13:28:19 +0100

    new test case

--- tests/Feature/PublishingEventToTopicTest.php
+++ tests/Feature/PublishingEventToTopicTest.php
@@ -6,7 +6,6 @@ use App\Jobs\PublishSubscriptionJob;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Bus;
-use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
 class PublishingEventToTopicTest extends TestCase
@@ -16,16 +15,10 @@ class PublishingEventToTopicTest extends TestCase
      *
      * @return void
      */
-    // public function test_topic_publishing_cannot_be_done_with_an_empty_body()
-    // {
-    //     $response = $this->postJson('/publish/task');
-    //     $response->assertStatus(200);
-    // }
-
     public function test_topic_publishing_can_be_done()
     {
         Bus::fake();
-        
+
         $topic = "task";
         $payload = [
             "message" => "Hello Chief"
